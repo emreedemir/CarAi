38251aa7b27db117768894f0459e0c1ab83b7748
Emre Edemir <31360356+emreedemir@users.noreply.github.com>
2020-12-02 21:22:38 +0300

    initial commit

---
+++ Assets/Car.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+public class Car : MonoBehaviour
+{
+
+    public Transform currentTarget;
+
+    public WayPoint wayPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wayPoint = FindObjectOfType<WayPoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (wayPoint.points == null)
+        {
+            return;
+        }
+        Vector3 nextPoint;
+
+        Sensor();
+
+        if (currentTarget == null)
+        {
+
+            currentTarget = GetShortestPoint(wayPoint.points, currentTarget);
+        }
+        else
+        {
+            if (Vector3.Distance(transform.position, currentTarget.position) < 2)
+            {
+                currentTarget = GetShortestPoint(wayPoint.points, currentTarget);
+            }
+        }
+
+        if (currentTarget != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, currentTarget.position, 10 * Time.deltaTime);
+
+            transform.rotation = Quaternion.LookRotation(currentTarget.position - transform.position, transform.forward);
+        }
+    }
+
+    public Transform GetShortestPoint(List<Transform> points, Transform current)
+    {
+
+        if (current == null)
+        {
+            Vector3 carPosition = transform.position;
+
+            float distance = Mathf.Infinity;
+
+            Transform target = null;
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                if (currentTarget != points[i])
+                {
+                    float newDistance = Vector3.Distance(transform.position, points[i].position);
+
+                    if (newDistance < distance)
+                    {
+                        distance = newDistance;
+
+                        target = points[i];
+                    }
+                }
+            }
+
+            return target;
+        }
+        else
+        {
+            int index = points.IndexOf(current);
+
+            if (index + 1 == points.Count)
+            {
+                index = 0;
+            }
+            else
+            {
+                index++;
+            }
+
+            return points[index];
+        }
+    }
+
+
+    public void Sensor()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, Quaternion.AngleAxis(-10 * i, transform.up) * transform.forward, out hit, 5))
+            {
+
+            }
+            else
+            {
+                Debug.DrawLine(transform.position, transform.position + Quaternion.AngleAxis(-10 * i, transform.up) * transform.forward * 5, Color.red);
+            }
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, Quaternion.AngleAxis(10 * i, transform.up) * transform.forward, out hit, 5))
+            {
+
+            }
+            else
+            {
+                Debug.DrawLine(transform.position, transform.position + Quaternion.AngleAxis(10 * i, transform.up) * transform.forward * 5, Color.red);
+            }
+        }
+
+    }
+}
---
+++ Assets/WayPoint.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+public class WayPoint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<Transform> points;
+    private void Start()
+    {
+        points = transform.OfType<Transform>().ToList();
+
+        StartCoroutine(CreateAllSprites(points));
+    }
+    public IEnumerator CreateAllSprites(List<Transform> points)
+    {
+        Debug.Log(points.Count);
+
+        for (int i = 0; i < points.Count; i++)
+        {
+            Debug.Log(i);
+
+            if (i + 1 == points.Count)
+            {
+
+                Transform prev = points[i - 1];
+
+                Transform current = points[i];
+
+                Transform next = points[0];
+
+                CreatePathSprit(current, next, prev);
+            }
+            else
+            {
+                Transform prev;
+
+                if (i != 0)
+                {
+                    prev = points[i - 1];
+                }
+                else
+                {
+                    prev = points[points.Count - 1];
+                }
+
+                Transform current = points[i];
+
+                Transform next = points[i + 1];
+
+                CreatePathSprit(current, next, prev);
+            }
+
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    private void CreatePathSprit(Transform current, Transform next, Transform prev)
+    {
+
+        Vector3 directionPrev = prev.position - transform.position;
+
+        Vector3 direction = (next.transform.position - current.transform.position) + directionPrev;
+
+        Vector3 left = /*Quaternion.AngleAxis(-90, transform.up) * direction.normalized * 3 */ 3 * direction.normalized + current.position;
+
+        Vector3 right =/* Quaternion.AngleAxis(90, transform.up) * direction.normalized * 3*/Quaternion.AngleAxis(-180, transform.up) * direction.normalized + current.position;
+
+        GameObject leftOb = Instantiate(new GameObject("Sprit1"));
+
+        GameObject rigtOb = Instantiate(new GameObject("Sprit2"));
+
+        leftOb.transform.position = left;
+
+        rigtOb.transform.position = right;
+
+        leftOb.transform.SetParent(current);
+
+        rigtOb.transform.SetParent(current);
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < points.Count; i++)
+        {
+            Transform next;
+
+            if (i + 1 == points.Count)
+            {
+                next = points[0];
+            }
+            else
+            {
+                next = points[i + 1];
+            }
+
+            Debug.DrawLine(points[i].position, next.position);
+
+            Debug.DrawLine(points[i].GetChild(0).position, next.GetChild(0).position, Color.red);
+
+            Debug.DrawLine(points[i].GetChild(1).position, next.GetChild(1).position, Color.red);
+        }
+    }
+
+}
